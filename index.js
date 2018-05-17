4c8df3856aeb0b7c3849e3b68cb19689ce02cbb1
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-17 14:44:30 -0400

    add hw to an object

--- index.js
+++ index.js
@@ -1,77 +1,24 @@
-const form=document.querySelector('#userForm')
+const app = {
+    init: function(formSelector){
+        this.max = 0
 
+        document
+        .querySelector(formSelector)
+        .addEventListener('submit', (ev)=> {
+            ev.preventDefault()
+            this.handleSubmit(ev)
+        })
+    },
 
-
-
-
-const renderListItem = function(hero,ability)
-    {
-        
-       const Item = document.createElement('li')
-      
-       
-       const name = document.createElement('li')
-       name.textContent = hero
-       const button = document.createElement("button")
-       button.innerHTML = "X"
-       name.appendChild(button)
-       
-       Item.appendChild(name)
-
-       //const description = document.createElement('dd')
-      // description.textContent = ability
-        
-
-      document.getElementById("users").addEventListener("click", removeItem());   
-        return Item
-       
-      
-        
+    handleSubmit: function(ev){
+        ev.preventDefault()
+        const f = ev.target
+        const hw = {
+            id:++this.max,
+            name: f.hwName.value,
         }
-    
-
-const renderList = function(data)
-{
-    const list = document.createElement('dl')
-     const labels = Object.keys(data)
-    labels.forEach(label =>
-    {
-       const item = renderListItem(label,data[label])
-       list.appendChild(item)
-    })
-
-    return list
+        f.reset()
+    },
 }
-   
-const handleSubmit = function(ev){
-    ev.preventDefault()
-    
-    const f = ev.target
 
-    const user ={
-        Name:f.heroName.value,
-        
-        
-    }
-
-          
-
-    const users=document.querySelector('#users')
-    users.appendChild(renderList(user))
-    
-    f.reset()
-    f.heroName.focus()
-}
-function removeItem(){
-        delete user.Name 
-    }
-
-    
-    
-
- form.addEventListener('submit', handleSubmit )
- 
-
-
-
-
+app.init('#hwForm')
